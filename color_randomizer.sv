ebaf8d57f35f12c9fa3809dde54c4df7af337fbc
Eli Foerst <97817592+efoerst@users.noreply.github.com>
2023-05-31 23:55:56 -0700

    Update color_randomizer.sv

--- color_randomizer.sv
+++ color_randomizer.sv
@@ -25,91 +25,54 @@ module color_randomizer(input wire clk, input rst, output reg [2:0] random_color
     end
 
     always_comb begin
-        if (state[2:0] == 3'd0) begin
+        if (state[2:0] < 3'd2) begin
             random_color[0] = PURPLE;
-        end else if (state[2:0] < 3'd2)  begin
+        end else if (state[2:0] < 3'd4)  begin
             random_color[0] = ORANGE;
-        end else if (state[2:0] < 3'd4) begin
+        end else begin
             random_color[0] = YELLOW;
-        end else if (state[2:0] < 3'd5) begin
-            random_color[0] = BLUE;
-        end else if (state[2:0] < 3'd7) begin
-            random_color[0] = RED;
-        end else begin
-            random_color[0] = GREEN;
-		  end 
+        end 
 		  
-        if (state[3:1] == 3'd0) begin
+        if (state[3:1] < 3'd2) begin
             random_color[1] = PURPLE;
-        end else if (state[3:1] < 3'd2)  begin
+        end else if (state[3:1] < 3'd4)  begin
             random_color[1] = ORANGE;
-        end else if (state[3:1] < 3'd4) begin
+        end else begin
             random_color[1] = YELLOW;
-        end else if (state[3:1] < 3'd5) begin
-            random_color[1] = BLUE;
-        end else if (state[3:1] < 3'd7) begin
-            random_color[1] = RED;
-        end else begin
-            random_color[1] = GREEN;
-		  end 	
+        end 
 		  
-        if (state[4:2] == 3'd0) begin
+        if (state[4:2] < 3'd2) begin
             random_color[2] = PURPLE;
-        end else if (state[4:2] < 3'd2)  begin
+        end else if (state[4:2] < 3'd4)  begin
             random_color[2] = ORANGE;
-        end else if (state[4:2] < 3'd4) begin
+        end else begin
             random_color[2] = YELLOW;
-        end else if (state[4:2] < 3'd5) begin
-            random_color[2] = BLUE;
-        end else if (state[4:2] < 3'd7) begin
-            random_color[2] = RED;
-        end else begin
-            random_color[2] = GREEN;
-		  end 
-        if (state[5:3] == 3'd0) begin
-            random_color[3] = PURPLE;
-        end else if (state[5:3] < 3'd2)  begin
-            random_color[3] = ORANGE;
-        end else if (state[5:3] < 3'd4) begin
-            random_color[3] = YELLOW;
-        end else if (state[5:3] < 3'd5) begin
-            random_color[3] = BLUE;
-        end else if (state[5:3] < 3'd7) begin
-            random_color[3] = RED;
-        end else begin
-            random_color[3] = GREEN;
 		  end 
 		  
-		  if (state[6:4] == 3'd0) begin
+        if (state[5:3] == 3'd2) begin
             random_color[3] = PURPLE;
-        end else if (state[6:4] < 3'd2)  begin
+        end else if (state[5:3] < 3'd4)  begin
             random_color[3] = ORANGE;
-        end else if (state[6:4] < 3'd4) begin
-            random_color[3] = YELLOW;
-        end else if (state[6:4] < 3'd5) begin
-            random_color[3] = BLUE;
-        end else if (state[6:4] < 3'd7) begin
-            random_color[3] = RED;
         end else begin
-            random_color[3] = GREEN;
-		  end 
+            random_color[3] = YELLOW;
+        end
 		  
-		  if (state[7:5] == 3'd0) begin
+		  if (state[6:4] < 3'd2) begin
+            random_color[3] = PURPLE;
+        end else if (state[6:4] < 3'd4)  begin
+            random_color[3] = ORANGE;
+        end else begin
+            random_color[3] = YELLOW;
+        end 
+		  
+		  if (state[7:5] < 3'd2) begin
             random_color[4] = PURPLE;
-        end else if (state[7:5] < 3'd2)  begin
+        end else if (state[7:5] < 3'd4)  begin
             random_color[4] = ORANGE;
-        end else if (state[7:5] < 3'd4) begin
-            random_color[4] = YELLOW;
-        end else if (state[7:5] < 3'd5) begin
-            random_color[4] = BLUE;
-        end else if (state[7:5] < 3'd7) begin
-            random_color[4] = RED;
         end else begin
-            random_color[4] = GREEN;
-		  end 
+            random_color[4] = YELLOW;
+        end 
 		  
     end
-
-    // assign randOut = {1'b0, state[2:0]} + 1; // use the LFSR output as random number
 
 endmodule
