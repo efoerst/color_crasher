43b52acccd84c7cbe32348ee956fc6281a343c0d
Eli Foerst <97817592+efoerst@users.noreply.github.com>
2023-06-01 17:46:33 -0700

    Update game_interpreter.sv
    One bullet

--- game_interpreter.sv
+++ game_interpreter.sv
@@ -2,26 +2,23 @@ module game_interpreter(
     // Inputs
     input[3:0] blockPos,
     input[2:0] ddState [0:4][0:5],
-    input[1:0] bullState [0:2],
-    input[3:0] bullX [0:2],
-    input[3:0] bullY [0:2],
+    input[1:0] bullState,
+    input[3:0] bullX,
+    input[3:0] bullY,
 
     // Outputs
     output reg[3:0] sub_blockieee_pos,
     output reg[11:0] sub_ddavers [0:4][0:5],
-    output reg[11:0] sub_bulletBillColor [0:2],
-    output reg[3:0] sub_bulletBillXLoc [0:2],
-    output reg[3:0] sub_bulletBillYLoc [0:2]
+    output reg[11:0] sub_bulletBillColor,
+    output reg[3:0] sub_bulletBillXLoc,
+    output reg[3:0] sub_bulletBillYLoc
 );
 
-// Parameter values
-
-// Bullet Bill States
+// Parameters
 localparam BBDNE = 2'd0;
 localparam EBLUE = 2'd1;
 localparam ERED = 2'd2;
 localparam EGREEN = 2'd3;
-// Ddaver States
 localparam DDNE = 3'd0;
 localparam PURPLE = 3'd1;
 localparam ORANGE = 3'd2;
@@ -29,6 +26,7 @@ localparam YELLOW = 3'd3;
 localparam BLUE = 3'd4;
 localparam RED = 3'd5;
 localparam GREEN = 3'd6;
+
 // Integer Values
 integer i;
 integer j;
@@ -72,27 +70,25 @@ always_comb begin
     end
 
     // Bullet Bill colors + positions
-    for (i = 0; i < 3; i = i + 1) begin
-        sub_bulletBillXLoc[i] = bullX[i];
-        sub_bulletBillYLoc[i] = bullY[i];
-        case (bullState[i])
-            BBDNE: begin
-                sub_bulletBillColor[i] = {4'd0, 4'd0, 4'd0};
-            end
-            EBLUE: begin
-                sub_bulletBillColor[i] = {4'd0, 4'd0, 4'd15};
-            end
-            ERED: begin
-                sub_bulletBillColor[i] = {4'd15, 4'd0, 4'd0};
-            end
-            EGREEN: begin
-                sub_bulletBillColor[i] = {4'd0, 4'd15, 4'd0};
-            end
-            default: begin
-                sub_bulletBillColor[i] = {4'd0, 4'd0, 4'd0};
-            end
-        endcase
-    end
+    sub_bulletBillXLoc = bullX;
+    sub_bulletBillYLoc = bullY;
+    case (bullState)
+        BBDNE: begin
+            sub_bulletBillColor = {4'd0, 4'd0, 4'd0};
+        end
+        EBLUE: begin
+            sub_bulletBillColor = {4'd0, 4'd0, 4'd15};
+        end
+        ERED: begin
+            sub_bulletBillColor = {4'd15, 4'd0, 4'd0};
+        end
+        EGREEN: begin
+            sub_bulletBillColor = {4'd0, 4'd15, 4'd0};
+        end
+        default: begin
+            sub_bulletBillColor = {4'd0, 4'd0, 4'd0};
+        end
+    endcase
 end
 
 endmodule
